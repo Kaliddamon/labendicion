9a24d19a8b0c1736cfcf917f784a417f413950d3
Cristian Sánchez <cristian.sanchez3@udea.edu.co>
2026-05-13 18:09:11 -0500

    Actualización de despliegue frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function figmaAssetResolver() {
   return {
@@ -28,10 +30,7 @@ export default defineConfig({
   },
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
@@ -42,6 +41,7 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  // @ts-expect-error Vitest test config
   test: {
     environment: 'jsdom',
     globals: true,
