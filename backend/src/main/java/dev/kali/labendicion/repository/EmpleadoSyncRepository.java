91dd723f980ffaaad5aa3b0c6fe3dbefee2e6a84
Cristian Sánchez <cristian.sanchez3@udea.edu.co>
2026-06-01 22:09:17 -0500

    Eliminar archivos antiguos con sufijo Sync

--- backend/src/main/java/dev/kali/labendicion/repository/EmpleadoSyncRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.kali.labendicion.repository;
-
-import dev.kali.labendicion.domain.entity.EmpleadoSync;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpleadoSyncRepository extends JpaRepository<EmpleadoSync, String> {
-}
--- backend/src/main/java/dev/kali/labendicion/repository/PasoProduccionSyncRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.kali.labendicion.repository;
-
-import dev.kali.labendicion.domain.entity.PasoProduccionSync;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PasoProduccionSyncRepository extends JpaRepository<PasoProduccionSync, String> {
-    List<PasoProduccionSync> findByProductoSyncId(String productoSyncId);
-}
