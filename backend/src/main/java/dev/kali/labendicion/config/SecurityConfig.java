9339a87abd0e32bbdc8df85b1db9d6c39e2061b0
Cristian Sánchez <cristian.sanchez3@udea.edu.co>
2026-07-31 13:13:14 -0500

    Fix: CORS

--- backend/src/main/java/dev/kali/labendicion/config/SecurityConfig.java
+++ backend/src/main/java/dev/kali/labendicion/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Autowired
     private GoogleTokenFilter googleTokenFilter;
 
-    @Value("${app.cors.allowed-origins:http://localhost:5173,http://127.0.0.1:5173,https://labendicion.vercel.app}")
+    @Value("${CORS_ALLOWED_ORIGINS:http://localhost:5173,http://127.0.0.1:5173,https://labendicion.vercel.app,https://labendicion-beta.vercel.app}")
     private String allowedOrigins;
 
     @Bean
